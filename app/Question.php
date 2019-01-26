de40c2f7a71da62f23feb782c11f19c672b00025
jahangirripon <ripon.workable@gmail.com>
2019-01-26 09:46:28 +0600

    prepopulate users and questions using model factory

--- app/Question.php
+++ app/Question.php
@@ -8,9 +8,14 @@ class Question extends Model
 {   
     protected $fillable = ['title', 'body'];
 
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
     }
 }
