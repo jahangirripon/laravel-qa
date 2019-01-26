add248b4ee00e0008f5f604816d7c75a98d26d92
jahangirripon <ripon.workable@gmail.com>
2019-01-26 08:51:56 +0600

    changed user model

---
+++ app/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{   
+    protected $fillable = ['title', 'body'];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -10,21 +10,22 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
