b2750f6b2de0b398d6f2836bc63b26e93caf7fc9
jahangirripon <ripon.workable@gmail.com>
2019-01-31 19:09:06 +0600

    question create form

---
+++ resources/views/questions/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>Ask Question</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back</a>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div class="card-body">
+                  <form action="{{ route('questions.store') }}" method="post">
+                    
+                    <div class="form-group">
+                      <label for="question-title">Question Title</label>
+                      <input type="text" name="title" id="question-title" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="question-body">Question Body</label>
+                      <textarea name="body" id="question-body" cols="30" rows="10" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                      <button type="submit" class="btn btn-outline-primary btn-lg">Ask question</button>
+                    </div>
+
+                  </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
